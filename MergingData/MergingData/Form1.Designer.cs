914b63280a17c3746c29352b8817a3e3fb812046
kysnail <TracingLife@gmail.com>
2012-03-16 11:25:16 +0800

    initial import of MergingData project.

---
+++ MergingData/MergingData/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace MergingData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnInputDirectory = new System.Windows.Forms.Button();
+            this.fbdInput = new System.Windows.Forms.FolderBrowserDialog();
+            this.tbxInputDirectory = new System.Windows.Forms.TextBox();
+            this.btnOutputDirectory = new System.Windows.Forms.Button();
+            this.tbxOutputDirectory = new System.Windows.Forms.TextBox();
+            this.fbdOutput = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnMerginData = new System.Windows.Forms.Button();
+            this.grpSelectPath = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.hToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grpSelectPath.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnInputDirectory
+            // 
+            this.btnInputDirectory.Location = new System.Drawing.Point(500, 47);
+            this.btnInputDirectory.Name = "btnInputDirectory";
+            this.btnInputDirectory.Size = new System.Drawing.Size(75, 23);
+            this.btnInputDirectory.TabIndex = 0;
+            this.btnInputDirectory.Text = "输入文件夹";
+            this.btnInputDirectory.UseVisualStyleBackColor = true;
+            this.btnInputDirectory.Click += new System.EventHandler(this.btnInputDirectory_Click);
+            // 
+            // tbxInputDirectory
+            // 
+            this.tbxInputDirectory.Location = new System.Drawing.Point(19, 49);
+            this.tbxInputDirectory.Name = "tbxInputDirectory";
+            this.tbxInputDirectory.Size = new System.Drawing.Size(454, 21);
+            this.tbxInputDirectory.TabIndex = 1;
+            // 
+            // btnOutputDirectory
+            // 
+            this.btnOutputDirectory.Location = new System.Drawing.Point(500, 97);
+            this.btnOutputDirectory.Name = "btnOutputDirectory";
+            this.btnOutputDirectory.Size = new System.Drawing.Size(75, 23);
+            this.btnOutputDirectory.TabIndex = 2;
+            this.btnOutputDirectory.Text = "输出文件夹";
+            this.btnOutputDirectory.UseVisualStyleBackColor = true;
+            this.btnOutputDirectory.Click += new System.EventHandler(this.btnOutputDirectory_Click);
+            // 
+            // tbxOutputDirectory
+            // 
+            this.tbxOutputDirectory.Location = new System.Drawing.Point(19, 98);
+            this.tbxOutputDirectory.Name = "tbxOutputDirectory";
+            this.tbxOutputDirectory.Size = new System.Drawing.Size(454, 21);
+            this.tbxOutputDirectory.TabIndex = 3;
+            // 
+            // btnMerginData
+            // 
+            this.btnMerginData.Location = new System.Drawing.Point(272, 206);
+            this.btnMerginData.Name = "btnMerginData";
+            this.btnMerginData.Size = new System.Drawing.Size(75, 23);
+            this.btnMerginData.TabIndex = 4;
+            this.btnMerginData.Text = "合并数据";
+            this.btnMerginData.UseVisualStyleBackColor = true;
+            this.btnMerginData.Click += new System.EventHandler(this.btnMerginData_Click);
+            // 
+            // grpSelectPath
+            // 
+            this.grpSelectPath.AutoSize = true;
+            this.grpSelectPath.Controls.Add(this.tbxInputDirectory);
+            this.grpSelectPath.Controls.Add(this.btnInputDirectory);
+            this.grpSelectPath.Controls.Add(this.tbxOutputDirectory);
+            this.grpSelectPath.Controls.Add(this.btnOutputDirectory);
+            this.grpSelectPath.Location = new System.Drawing.Point(12, 40);
+            this.grpSelectPath.Name = "grpSelectPath";
+            this.grpSelectPath.Size = new System.Drawing.Size(595, 144);
+            this.grpSelectPath.TabIndex = 5;
+            this.grpSelectPath.TabStop = false;
+            this.grpSelectPath.Text = "选择路径";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.hToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(618, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(12, 20);
+            // 
+            // hToolStripMenuItem
+            // 
+            this.hToolStripMenuItem.Name = "hToolStripMenuItem";
+            this.hToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.hToolStripMenuItem.Text = "&Help";
+            this.hToolStripMenuItem.Click += new System.EventHandler(this.hToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 251);
+            this.Controls.Add(this.grpSelectPath);
+            this.Controls.Add(this.btnMerginData);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MerginData";
+            this.grpSelectPath.ResumeLayout(false);
+            this.grpSelectPath.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInputDirectory;
+        private System.Windows.Forms.FolderBrowserDialog fbdInput;
+        private System.Windows.Forms.TextBox tbxInputDirectory;
+        private System.Windows.Forms.Button btnOutputDirectory;
+        private System.Windows.Forms.TextBox tbxOutputDirectory;
+        private System.Windows.Forms.FolderBrowserDialog fbdOutput;
+        private System.Windows.Forms.Button btnMerginData;
+        private System.Windows.Forms.GroupBox grpSelectPath;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem hToolStripMenuItem;
+    }
+}
+
---
+++ MergingData/MergingData/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace MergingData
+{
+    public partial class Form1 : Form
+    {
+        DialogResult resultInput = DialogResult.None;
+        DialogResult resultOnput = DialogResult.None;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInputDirectory_Click(object sender, EventArgs e)
+        {
+            resultInput = fbdInput.ShowDialog();
+            try 
+            {
+                if (resultInput == DialogResult.OK)
+                {
+                    tbxInputDirectory.Text = fbdInput.SelectedPath;
+                }
+            }  
+            catch (Exception exp)
+            {
+                MessageBox.Show("错误的路径");
+            }
+        }
+
+        private void btnOutputDirectory_Click(object sender, EventArgs e)
+        {
+            DialogResult resultOutput = fbdOutput.ShowDialog();
+
+            try
+            {
+                if (resultOutput == DialogResult.OK)
+                {
+                    tbxOutputDirectory.Text = fbdOutput.SelectedPath;
+                }
+            }
+            catch(Exception exp)
+            {
+                MessageBox.Show("错误的路径");
+            }
+        }
+
+        private void btnMerginData_Click(object sender, EventArgs e)
+        {
+            if (tbxInputDirectory.Text != "" && tbxOutputDirectory.Text != "" && Directory.Exists(tbxInputDirectory.Text) && Directory.Exists(tbxOutputDirectory.Text))
+            {
+                ApplicationClass app = new ApplicationClass();
+                Workbook book = null;
+                Worksheet sheet = null;
+                Range range = null;
+                Dictionary<string, string> dic = new Dictionary<string, string>();
+                Dictionary<string, object> outdic = new Dictionary<string, object>();
+
+                //File file = null;
+                string[] filename = null;
+
+                //object[,] values = null;
+
+                try
+                {
+                    app.Visible = false;
+                    app.ScreenUpdating = false;
+                    app.DisplayAlerts = false;
+
+                    //string execPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
+                    //filename = Directory.GetFiles(execPath.Substring(6), "*.xls");
+                    filename = Directory.GetFiles(tbxInputDirectory.Text, "*.xls");
+                    //Array.Sort(filename);
+
+                    ArrayList al = new ArrayList();
+
+                    foreach (string f in filename)
+                    {
+                        Console.WriteLine(f.ToString());
+                        book = app.Workbooks.Open(f,
+                              Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value,
+                              Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value,
+                              Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+
+                        sheet = (Worksheet)book.Worksheets[1];
+                        range = sheet.get_Range("A32", "B132");
+                        object[,] values = (object[,])range.Value2;
+                        //values = (object[,])range.Value2;
+
+                        Console.WriteLine("Row Count: " + values.GetLength(0).ToString());
+                        Console.WriteLine("Col Count: " + values.GetLength(1).ToString());
+
+                        Console.Write("\t");
+                        for (int j = 1; j <= values.GetLength(1); j++)
+                        {
+                            Console.Write("{0}\t", j);
+                        }
+
+                        #region test
+                        Console.WriteLine();
+                        for (int i = 1; i <= values.GetLength(0); i++)
+                        {
+                            Console.Write("{0}\t", i);
+
+                            string dicKey = "";
+                            string dicValue = "";
+                            for (int j = 1; j <= values.GetLength(1); j++)
+                            {
+                                switch (j)
+                                {
+                                    case 1:
+                                        dicKey = values[i, j].ToString();
+                                        break;
+                                    case 2:
+                                        dicValue = values[i, j].ToString();
+                                        break;
+                                }
+                                Console.Write("{0}\t", values[i, j]);
+                            }
+                            dic.Add(dicKey, dicValue);
+                            dicKey = dicValue = "";
+                            Console.WriteLine();
+                        #endregion test
+                        }
+                        Console.WriteLine("---------------------------------");
+
+                        al.Clear();
+                        al.Add(210);
+                        al.Add(222);
+                        al.Add(228);
+                        al.Add(254);
+                        al.Add(260);
+                        al.Add(280);
+                        al.Add(296);
+                        for (int i = 0; i != al.Count; i++)
+                        {
+                            Console.WriteLine("{0}\t{1}", al[i], dic[al[i].ToString()]);
+                        }
+
+                        outdic.Add(f.Split('\\')[f.Split('\\').Length - 1], dic);
+                        dic = new Dictionary<string, string>();
+                        // dic.Clear()  cannot using this method.
+                    }
+                    Workbook outbooks = app.Workbooks.Add(Missing.Value);
+                    Worksheet outsheet = (Worksheet)outbooks.Worksheets[1];
+
+                    string[,] insvalue = new string[outdic.Count, 1];
+                    string[,] inscvalue = new string[outdic.Count, al.Count];
+                    for (int i = 0; i != outdic.Count; i++)
+                    {
+                        //insvalue[i, 0] = outdic.Keys[i];
+                    }
+
+                    int c = 0;
+                    // int d = 0;
+                    foreach (KeyValuePair<string, object> d in outdic)
+                    {
+                        // Console.WriteLine(d.Key.ToString() + d.Value.ToString());
+                        insvalue[c, 0] = d.Key;
+                        c++;
+                        //Console.WriteLine(c + d.Key);
+                    }
+
+                    outsheet.get_Range("A1", "A" + filename.Length).set_Value(XlRangeValueDataType.xlRangeValueDefault, insvalue);
+
+                    Array.Clear(insvalue, 0, insvalue.Length);
+                    Console.WriteLine("1111111111111111111");
+
+                    int row = 0;
+                    foreach (KeyValuePair<string, object> p in outdic)
+                    {
+                        // Console.WriteLine(d.Key.ToString() + d.Value.ToString());
+
+                        //foreach (KeyValuePair<string, string> bb in (Dictionary<string, string>)outdic[d.Key])
+                        //{
+                        //    //Console.WriteLine(c + "\t" + bb.Key);
+                        //    // Console.WriteLine("{0}\t{1}", al[i], dic[al[i].ToString()]);
+                        //    inscvalue[0, d] = bb[]
+                        //    //for (int i = 0; i != al.Count; i++)
+                        //    //{
+                        //    //    Console.WriteLine("{0}\t{1}", al[i], bb[al[i].ToString()]);
+                        //    //}
+                        //}
+                        Dictionary<string, string> dddd = (Dictionary<string, string>)p.Value;
+                        // Console.WriteLine(dddd["300"]);
+                        //for (int i = 0; i != outdic.Count; i++)
+                        //{
+                        for (int j = 0; j != al.Count; j++)
+                        {
+                            Console.WriteLine("---->   {0}\t{1}", al[j], dddd[al[j].ToString()]);
+                            inscvalue[row, j] = dddd[al[j].ToString()];
+                        }
+                        row++;
+                        //}
+                        outsheet.get_Range("B1", "H" + filename.Length).set_Value(XlRangeValueDataType.xlRangeValueDefault, inscvalue);
+                    }
+
+
+                    //outbooks.Save();
+                    outbooks.SaveAs(tbxOutputDirectory.Text + "\\Output.xls",
+                                    Microsoft.Office.Interop.Excel.XlFileFormat.xlXMLSpreadsheet,
+                                    System.Reflection.Missing.Value,
+                                    System.Reflection.Missing.Value,
+                                    System.Reflection.Missing.Value,
+                                    System.Reflection.Missing.Value,
+                                    Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+                                    System.Reflection.Missing.Value,
+                                    System.Reflection.Missing.Value,
+                                    System.Reflection.Missing.Value,
+                                    System.Reflection.Missing.Value,
+                                    System.Reflection.Missing.Value);
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine(exp);
+                }
+                finally
+                {
+                    range = null;
+                    sheet = null;
+                    if (book != null)
+                        book.Close(false, Missing.Value, Missing.Value);
+                    book = null;
+                    if (app != null)
+                        app.Quit();
+                    app = null;
+                }
+            }
+            else
+            {
+                MessageBox.Show("请输入正确的文件夹路径");
+            }
+        }
+
+        private void hToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Version 12");
+        }
+    }
+}
